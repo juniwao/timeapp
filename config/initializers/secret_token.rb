d14659c93f15cbcafd427a90d87508ad64179ad6
juniwao <juniwao@hotmail.com>
2015-10-15 11:50:53 +0200

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeapp::Application.config.secret_key_base = 'cd2c3d256a00e71b77c4e513a8c4763f9010bf3113ae8a472f90e8b50542982fbdce20239ea224d22293c72848326db3c5eb5d1fd4890d95721a0a51ef12c030'
